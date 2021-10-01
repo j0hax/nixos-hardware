10f31d6480002a6d282a7207c2455650eed9b848
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-10-01 02:48:48 +0200

    Remove OpenCL and amdvlk packages

--- common/gpu/amd/default.nix
+++ common/gpu/amd/default.nix
@@ -3,16 +3,6 @@
 {
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
-  
-  hardware.opengl.extraPackages = with pkgs; [
-    rocm-opencl-icd
-    rocm-opencl-runtime
-    amdvlk
-  ];
-
-  hardware.opengl.extraPackages32 = with pkgs; [
-    driversi686Linux.amdvlk
-  ];
 
   hardware.opengl = {
     driSupport = lib.mkDefault true;
